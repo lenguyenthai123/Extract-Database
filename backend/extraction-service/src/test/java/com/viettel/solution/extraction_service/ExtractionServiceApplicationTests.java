0cc06e47ff81970c08c9517b81d2a19538445cf3
lenguyenthai123 <21127162@student.hcmus.edu.vn>
2024-05-23 15:52:43 +0700

    initialize Extraction Service

---
+++ backend/extraction-service/src/test/java/com/viettel/solution/extraction_service/ExtractionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viettel.solution.extraction_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExtractionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
