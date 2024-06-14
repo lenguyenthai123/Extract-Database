1de9d3e5c0b96d2262a006f72adcf9e7ca75290d
Le Nguyen Thai <21127162@student.hcmus.edu.vn>
2024-06-15 02:12:05 +0700

    deploy

--- backend/extraction-service/src/main/java/com/viettel/solution/extraction_service/config/RedisConfig.java
+++ backend/extraction-service/src/main/java/com/viettel/solution/extraction_service/config/RedisConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -13,6 +15,18 @@ import java.time.Duration;
 
 @Configuration
 public class RedisConfig {
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName("redis"); // Đặt host là 'redis'
+        redisStandaloneConfiguration.setPort(6379); // Đặt port là 6379
+        // Nếu bạn có username và password, bạn có thể đặt chúng ở đây
+        // redisStandaloneConfiguration.setUsername("your-username");
+        // redisStandaloneConfiguration.setPassword("your-password");
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
+    }
+
     @Bean
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig() //
--- frontend/src/app/env/environment.ts
+++ frontend/src/app/env/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:8080',
+  apiUrl: 'http://54.254.226.218:8080',
   // Các biến môi trường khác
 };
